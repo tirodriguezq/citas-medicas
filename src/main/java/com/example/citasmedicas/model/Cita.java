2837e4c98154227c0399c86ba728c381b60cbf61
tiffanyrodr <tiff18448@gmail.com>
2026-04-08 16:12:55 -0600

    complete project requirements with JDBC, roles and GraphQL

--- src/main/java/com/example/citasmedicas/model/Cita.java
+++ src/main/java/com/example/citasmedicas/model/Cita.java
@@ -1,5 +1,8 @@
 package com.example.citasmedicas.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
@@ -7,6 +10,9 @@ import org.springframework.data.relational.core.mapping.Table;
 import java.time.LocalDate;
 
 @Table("CITAS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cita {
 
     @Id
@@ -20,55 +26,4 @@ public class Cita {
 
     private LocalDate fecha;
     private String estado;
-
-    public Cita() {
-    }
-
-    public Cita(Long id, Long pacienteId, Long doctorId, LocalDate fecha, String estado) {
-        this.id = id;
-        this.pacienteId = pacienteId;
-        this.doctorId = doctorId;
-        this.fecha = fecha;
-        this.estado = estado;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getPacienteId() {
-        return pacienteId;
-    }
-
-    public Long getDoctorId() {
-        return doctorId;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setPacienteId(Long pacienteId) {
-        this.pacienteId = pacienteId;
-    }
-
-    public void setDoctorId(Long doctorId) {
-        this.doctorId = doctorId;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
 }
