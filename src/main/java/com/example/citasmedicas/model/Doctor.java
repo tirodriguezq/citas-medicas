34472f019e8fa1a960f338cbca1f044789815d58
tiffanyrodr <tiff18448@gmail.com>
2026-04-08 16:01:26 -0600

    migrate remaining models to lombok

--- src/main/java/com/example/citasmedicas/model/Doctor.java
+++ src/main/java/com/example/citasmedicas/model/Doctor.java
@@ -1,46 +1,19 @@
 package com.example.citasmedicas.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("DOCTORES")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Doctor {
 
     @Id
     private Long id;
     private String nombre;
     private String especialidad;
-
-    public Doctor() {
-    }
-
-    public Doctor(Long id, String nombre, String especialidad) {
-        this.id = id;
-        this.nombre = nombre;
-        this.especialidad = especialidad;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getEspecialidad() {
-        return especialidad;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
-    }
 }
--- src/main/java/com/example/citasmedicas/model/Paciente.java
+++ src/main/java/com/example/citasmedicas/model/Paciente.java
@@ -1,46 +1,19 @@
 package com.example.citasmedicas.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("PACIENTES")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Paciente {
 
     @Id
     private Long id;
     private String nombre;
     private String telefono;
-
-    public Paciente() {
-    }
-
-    public Paciente(Long id, String nombre, String telefono) {
-        this.id = id;
-        this.nombre = nombre;
-        this.telefono = telefono;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
 }
